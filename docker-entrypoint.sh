21617a6898656613bf1ed293c720f1d02bc86665
Tobias Salzmann <salzmann@talon.one>
2018-12-20 10:29:03 +0100

    added buildkite

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,7 @@ set -e
 apk update
 apk add expect cdrkit p7zip tar
 cd /build/
+if ! [ -e "alpine-virt-3.8.0-x86_64.iso" ]; then
+    wget http://dl-cdn.alpinelinux.org/alpine/v3.8/releases/x86_64/alpine-standard-3.8.1-x86_64.iso
+fi
 ./create-vm.exp
