969d052f4c851a86831484330013f9f29b596f3e
Tobias Salzmann <eun@users.noreply.github.com>
2019-02-08 15:00:34 +0100

    be more verbose, fix for decoding

--- .buildkite/create-image.sh
+++ .buildkite/create-image.sh
@@ -5,9 +5,11 @@ BUCKET_PATH="${BUCKET}/${BUILDKITE_PIPELINE_SLUG}-${BUILDKITE_BRANCH}${BUILDKITE
 
 gsutil cp disk.raw.tar.gz gs://$BUCKET_PATH
 gcloud compute --project={$PROJECT} \
-    images delete buildkite-3.5.1
+    images delete --quiet buildkite-agent-3-5-1
 
 gcloud compute --project={$PROJECT} \
-    images create buildkite-3.5.1 \
+    images create buildkite-agent-3-5-1 \
     --family=alpine \
-    --source-uri=https://$BUCKET_PATH
+    --source-uri=https://storage.googleapis.com/$BUCKET_PATH
+
+gsutil rm gs://$BUCKET_PATH
