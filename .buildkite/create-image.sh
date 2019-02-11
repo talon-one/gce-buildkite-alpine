03cdd5a7b3b36d0a2542a98286ba3d52c632a66c
Tobias Salzmann <eun@users.noreply.github.com>
2019-02-11 11:09:48 +0100

    fix project

--- .buildkite/create-image.sh
+++ .buildkite/create-image.sh
@@ -9,7 +9,7 @@ gcloud auth activate-service-account --key-file=$GCLOUD_CREDENTIAL_FILE
 BUCKET_PATH="${BUCKET}/${BUILDKITE_PIPELINE_SLUG}-${BUILDKITE_BRANCH}${BUILDKITE_BUILD_ID}.tar.gz"
 
 gsutil cp disk.raw.tar.gz gs://$BUCKET_PATH
-gcloud compute --project={$PROJECT} \
+gcloud compute --project=$PROJECT \
     images create buildkite-agent-3-5-1-$(date +"%F-%H-%m-%S") \
     --family=alpine \
     --source-uri=https://storage.googleapis.com/$BUCKET_PATH
