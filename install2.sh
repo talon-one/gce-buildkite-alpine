33c154487ad348cc840018466dc5d4ebf60deced
Tobias Salzmann <Eun@users.noreply.github.com>
2019-05-22 16:00:52 +0200

    speed up build time by installing some python dependencies (#4)

--- install2.sh
+++ install2.sh
@@ -94,7 +94,7 @@ rc-update add docker-credential-file default
 
 # install docker compose
 printf "\e[7mInstalling Docker Compose\e[0m\n"
-apk add python py-pip gcc libffi libffi-dev
+apk add python py-pip gcc libffi libffi-dev py-bcrypt py-cryptography py-pynacl
 pip install --upgrade pip
 pip install docker-compose
 
