f87fd39f1eb9606a6cab4499e398a87ab18bad2b
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2020-01-30 14:30:10 +0100

    update buildkite-agent to 3.19.0

--- install2.sh
+++ install2.sh
@@ -111,8 +111,8 @@ mkdir /etc/buildkite-agent 2>&1 || true
 mkdir /tmp/buildkite 2>&1 || true
 pushd /tmp/buildkite
 # download and install
-wget https://github.com/buildkite/agent/releases/download/v3.17.0/buildkite-agent-linux-amd64-3.17.0.tar.gz
-tar -xzf buildkite-agent-linux-amd64-3.17.0.tar.gz
+wget -O buildkite-agent.tar.gz https://github.com/buildkite/agent/releases/download/v3.19.0/buildkite-agent-linux-amd64-3.19.0.tar.gz
+tar -xzf buildkite-agent.tar.gz
 mv buildkite-agent /usr/sbin/buildkite-agent
 popd
 # setup permissions
